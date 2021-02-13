9e1bdcf67f7522a049a824d6eede5ff339c6ad53
Prajwal Gowda S <65961288+Prajwal518@users.noreply.github.com>
2021-02-13 13:45:51 +0530

    practice used expressions and made the year dynamic

--- src/index.js
+++ src/index.js
@@ -1,8 +1,18 @@
 //Create a react app from scratch.
+import React from "react";
+import ReactDom from "react-dom";
 //It should display 2 paragraph HTML elements.
 //The paragraphs should say:
-//Created by YOURNAME.
-//Copyright CURRENTYEAR.
-//E.g.
-//Created by Angela Yu.
-//Copyright 2019.
+//Created by YOURNAME.//Copyright CURRENTYEAR.//E.g.//Created by Angela Yu.//Copyright 2019.
+
+const name = "Prajwal Gowda S";
+const currentDate = new Date();
+const year = currentDate.getFullYear();
+
+ReactDom.render(
+  <div>
+    <p>Created by {name}</p>
+    <p>Copyright {year}</p>
+  </div>,
+  document.getElementById("root")
+);
